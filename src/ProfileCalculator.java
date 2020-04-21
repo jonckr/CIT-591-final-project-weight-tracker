c038ae3b2fddf14cd29252b966447bd4cec3d412
Jonathan Cheong <jonckr@gmail.com>
2020-04-20 22:14:12 -0500

    Updated Profile Calculator

--- src/ProfileCalculator.java
+++ src/ProfileCalculator.java
@@ -1,12 +1,18 @@
 import javax.swing.JOptionPane;
 import java.lang.Math;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class ProfileCalculator {
 	private double inactiveBMR; //BMR stands for Basal Metabolic Rate
 	private double dailyEnergyExpenditure;
-	private double poundsLose;
+	private double weightToLose;
 	private double totalPoundDeficit;
-	private int weeksLeft;
+	private double targetWeightToLose;
+	private int targetDays;
+	private int dailyCaloriesLoss;
+	private int dailyCaloricIntake;
 
 	/**
 	 * Calculate user's BMI using Metrics measures
@@ -29,7 +35,7 @@ public class ProfileCalculator {
 	 */
 	private double calculateInactiveBMR(String gender, double currentWeight, double currentHeight, int age) {
 		if ( gender.toLowerCase().equals("male") || gender.toLowerCase().equals("m") ) {
-			inactiveBMR = 66 + (6.23 * currentWeight) + (12.7 * currentHeight) - (6.8 * age);
+			inactiveBMR = 66 + (6.23 * currentWeight) + (12.7 * currentHeight) - (6.8 * age); //height is in inches
 		}
 		else if (gender.toLowerCase().equals("female") || gender.toLowerCase().equals("f")){
 			inactiveBMR = 655 + (4.35 * currentWeight) + (4.7 * currentHeight) - (4.7 * age);
@@ -41,25 +47,13 @@ public class ProfileCalculator {
 
 	}
 
-	//Add in method to calculate Activity level
-	/*
-	public float calcTDEE(String activityLevel) {
-		Map<String, float> activityLevelMultipliers = new Map<String, float>();
-		activityLevelMultipliers.put("Sedentary", 1.2);
-		activityLevelMultipliers.put("Lightly Active", 1.375);
-	    return activityLevelMultipliers.get(activityLevel) * calcBMR(...);
-	}
-    public static int calcTDEE(String selectedActivityLevel) {
-        return (int) Math.round(calcBMR(BmrMain.userAge, BmrMain.userHeight, BmrMain.userWeight) * BmrMain.activityMap.get(selectedActivityLevel));
-}
-*/
 	/**
 	 * Calculates the minimum number of calories to maintain current weight - Based off BMR and Activity Level
 	 * (In other words, this is the users Daily Calorie Needs)
 	 * @param activityLevel
 	 * @return
 	 */
-	public double calculateDailyEnergyExpenditure(int activityLevel) {
+	public double calculateDailyEnergyExpenditure(double inactiveBMR, int activityLevel) {
 
 		if (activityLevel == 1) { //Sendary BMR (little or no exercise, desk job)
 			dailyEnergyExpenditure = inactiveBMR * 1.2;
@@ -75,17 +69,72 @@ public class ProfileCalculator {
 		return dailyEnergyExpenditure;
 	}
 
-	//Figure out how to relate Daily Energy Expenditure to how much user intakes a day and what is his caloric deficit
-	public double calculateCalorieDeficit(double currentWeight, double targetWeight, double dailyEnergyExpenditure) {
-		poundsLose = currentWeight - targetWeight;
-		totalPoundDeficit = poundsLose * 3500;
-		return totalPoundDeficit;
-	}
-	//Add in method to calculate how many more weeks left
-	public int weeksToFinish(double totalPoundDeficit, int activityLevel) {
-		weeksLeft = (int) Math.ceil( ( totalPoundDeficit / (7 * activityLevel) ) ); // Rounds up whatever double value of weeks
-		return weeksLeft;
+	/**
+	 * This Method Calculates how much weight i need to lose to get to my targeted weight
+	 * @param currentWeight
+	 * @param targetWeight
+	 * @return
+	 */
+	public double calculateTargetWeightToLose(double currentWeight, double targetWeight) {
+		weightToLose = currentWeight - targetWeight; //How many lbs i would need to lose to get to my target weight
+		return weightToLose;
 	}
 
+
+	/**
+	 * Method calculates number of calories to lose to achieve targeted weight
+	 * @param currentWeight
+	 * @param targetWeight
+	 * @param dailyEnergyExpenditure
+	 * @return
+	 */
+	public double calculateCalorieDeficit(double currentWeight, double targetWeight, double dailyEnergyExpenditure) {
+		totalPoundDeficit = weightToLose * 3500; // Number of calories i need to lose to get to my target weight
+		return totalPoundDeficit;
+	}
+
+	/**
+	 * This Method calculates the total amount of calories a user needs to lose in order to achieve his target weight
+	 * @param totalPoundDeficit
+	 * @param targetDays
+	 * @return
+	 */
+	public int calculateDailyCalorieLoss(double totalPoundDeficit, int targetDays) {
+		dailyCaloriesLoss = (int) Math.round(totalPoundDeficit / targetDays); // Rounds to close calories
+		return dailyCaloriesLoss;
+	}
+
+	/**
+	 * This method calculates the daily calories a user can intake in order to achieve his target weight
+	 * @param dailyEnergyExpenditure
+	 * @return
+	 */
+	public int calculateDailyCaloricIntake(int dailyCaloriesLoss, double dailyEnergyExpenditure) {
+		dailyCaloricIntake = (int) Math.round(dailyEnergyExpenditure - dailyCaloriesLoss); // How much calories user can intake a day
+		return dailyCaloricIntake;
+	}
+
+	//Should we return a warning if dailyCaloricIntake is less than 50% of dailyEnergyExpenditure?
+		// EG: Be aware that you are intaking less than 50% of your daily energy expenditure and this might be unhealthy in the long run
+
+	/**
+	 * This method returns all the output required for the HTML output
+	 * @param weightToLose
+	 * @param targetDays
+	 * @param dailyEnergyExpenditure
+	 * @param dailyCaloriesLoss
+	 * @param dailyCaloricIntake
+	 * @return
+	 */
+	public String[] generateOutput(double weightToLose, int targetDays, double dailyEnergyExpenditure, int dailyCaloriesLoss, int dailyCaloricIntake) {
+		String [] myArrayOutput = new String [5];
+		myArrayOutput[0] = String.valueOf(weightToLose);
+		myArrayOutput[1] = String.valueOf(targetDays);
+		myArrayOutput[2] = String.valueOf(dailyEnergyExpenditure);
+		myArrayOutput[3] = String.valueOf(dailyCaloriesLoss);
+		myArrayOutput[4] = String.valueOf(dailyCaloricIntake);
+		return myArrayOutput;
+
+	}
 
 }
--- src/ProfileCalculatorTest.java
+++ src/ProfileCalculatorTest.java
@@ -7,14 +7,14 @@ class ProfileCalculatorTest {
 
 	@Test
 	void testCalculateBMI() {
-		
-		
+
+
 		//test1
 		double curWeight1 = 165.1;
 		double curHeight1 = 72.3;
 		double bmi1 = calculator.calculateBMI(curWeight1,curHeight1);
 		assertEquals(bmi1,22.10897,0.001);
-		
+
 		//test2
 		double curWeight2 = 200.13;
 		double curHeight2 = 88.7;
@@ -24,21 +24,37 @@ class ProfileCalculatorTest {
 
 	@Test
 	void testCalculateInactiveBMR() {
-		
+
 		//test1
 		String gender1 = "MALE";
 		double currentWeight1 = 165.1;
 		double currentHeight1 = 72.3;
 		int age1 = 24;
 		assertEquals(calculator.calculateInactiveBMR(gender1, currentWeight1, currentHeight1, age1),1849.58299,0.001);
-	
+
 		//test2
 		String gender2 = "f";
 		double currentWeight2 = 132.1;
 		double currentHeight2 = 63.3;
 		int age2 = 18;
 		assertEquals(calculator.calculateInactiveBMR(gender2, currentWeight2, currentHeight2, age2),1442.545,0.001);
-	
+
 	}
+
+		@Test
+		void calculateDailyEnergyExpenditure() {
+
+			//test1
+			double inactiveBMR = 1849.58299;
+			int activityLevel = 3;
+			assertEquals(calculator.calculateDailyEnergyExpenditure(inactiveBMR, activityLevel),2866.85,0.01);
+
+			//test2
+			//test1
+			double inactiveBMR = 1442.545;
+			int activityLevel = 1;
+			assertEquals(calculator.calculateDailyEnergyExpenditure(inactiveBMR, activityLevel),1731.05,0.01);
+
+		}
 
 }
